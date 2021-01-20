c9fdef31d2e667ba2987a5018761569504a3880d
Max Ribbans <ribbans.max@gmail.com>
2021-01-19 20:20:27 -0500

    Fix show_spec test

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Bulk Discount Index" do
-  it "displays links to all discounts" do
+RSpec.describe "Bulk Discount show" do
+  it "displays discount info" do
     merchant = create(:merchant)
     discount = create(:bulk_discount, merchant: merchant)
 
     visit merchant_bulk_discount_path(merchant, discount)
 
     expect(page).to have_link("#{(discount.discount * 100).round(2)}% off", href: merchant_bulk_discount_path(merchant, discount))
+    expect(page).to have_content(discount.threshold)
   end
 end
